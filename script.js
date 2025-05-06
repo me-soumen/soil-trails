a8d3c04682554663d871beaa471cb779c829468b
soumen <me.soumen02@gmail.com>
2025-05-06 23:48:45 +0530

    Updated home page design

---
+++ script.js
@@ -0,0 +1,454 @@
+const data = [
+  {
+    "code": "KA",
+    "state": "Andaman & Nicobar",
+    "samples": [
+      {
+        "id": "#1",
+        "place": "Hogenakkal Falls",
+        "date": "2025-02-08",
+        "time": "13:30",
+        "latitude": 12.110985864583052,
+        "longitude": 77.77479544773149,
+        "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+      },
+      {
+        "id": "#2",
+        "place": "Bangalore",
+        "date": "2025-05-08",
+        "time": "16:30",
+        "latitude": 12.110985864583052,
+        "longitude": 77.77479544773149,
+        "notes": "From KR Garden 2nd Main, Murugeshpalya"
+      },
+             {
+               "id": "#2",
+               "place": "Bangalore",
+               "date": "2025-05-08",
+               "time": "16:30",
+               "latitude": 12.110985864583052,
+               "longitude": 77.77479544773149,
+               "notes": "From KR Garden 2nd Main, Murugeshpalya"
+             },
+                    {
+                      "id": "#2",
+                      "place": "Bangalore",
+                      "date": "2025-05-08",
+                      "time": "16:30",
+                      "latitude": 12.110985864583052,
+                      "longitude": 77.77479544773149,
+                      "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                    },
+                           {
+                             "id": "#2",
+                             "place": "Bangalore",
+                             "date": "2025-05-08",
+                             "time": "16:30",
+                             "latitude": 12.110985864583052,
+                             "longitude": 77.77479544773149,
+                             "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                           },
+                                  {
+                                    "id": "#2",
+                                    "place": "Bangalore",
+                                    "date": "2025-05-08",
+                                    "time": "16:30",
+                                    "latitude": 12.110985864583052,
+                                    "longitude": 77.77479544773149,
+                                    "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                                  },
+                                         {
+                                           "id": "#2",
+                                           "place": "Bangalore",
+                                           "date": "2025-05-08",
+                                           "time": "16:30",
+                                           "latitude": 12.110985864583052,
+                                           "longitude": 77.77479544773149,
+                                           "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                                         },
+                                                {
+                                                  "id": "#2",
+                                                  "place": "Bangalore",
+                                                  "date": "2025-05-08",
+                                                  "time": "16:30",
+                                                  "latitude": 12.110985864583052,
+                                                  "longitude": 77.77479544773149,
+                                                  "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                                                },
+                                                       {
+                                                         "id": "#2",
+                                                         "place": "Bangalore",
+                                                         "date": "2025-05-08",
+                                                         "time": "16:30",
+                                                         "latitude": 12.110985864583052,
+                                                         "longitude": 77.77479544773149,
+                                                         "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                                                       },
+                                                              {
+                                                                "id": "#2",
+                                                                "place": "Bangalore",
+                                                                "date": "2025-05-08",
+                                                                "time": "16:30",
+                                                                "latitude": 12.110985864583052,
+                                                                "longitude": 77.77479544773149,
+                                                                "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                                                              }
+    ]
+  },
+  {
+      "code": "KA",
+      "state": "Karnataka",
+      "samples": [
+      ]
+    },
+    {
+        "code": "KA",
+        "state": "Karnataka",
+        "samples": [
+          {
+            "id": "#1",
+            "place": "Hogenakkal Falls",
+            "date": "2025-02-08",
+            "time": "13:30",
+            "latitude": 12.110985864583052,
+            "longitude": 77.77479544773149,
+            "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+          },
+          {
+            "id": "#2",
+            "place": "Bangalore",
+            "date": "2025-05-08",
+            "time": "16:30",
+            "latitude": 12.110985864583052,
+            "longitude": 77.77479544773149,
+            "notes": "From KR Garden 2nd Main, Murugeshpalya"
+          }
+        ]
+      },
+      {
+          "code": "KA",
+          "state": "Karnataka",
+          "samples": [
+            {
+              "id": "#1",
+              "place": "Hogenakkal Falls",
+              "date": "2025-02-08",
+              "time": "13:30",
+              "latitude": 12.110985864583052,
+              "longitude": 77.77479544773149,
+              "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+            },
+            {
+              "id": "#2",
+              "place": "Bangalore",
+              "date": "2025-05-08",
+              "time": "16:30",
+              "latitude": 12.110985864583052,
+              "longitude": 77.77479544773149,
+              "notes": "From KR Garden 2nd Main, Murugeshpalya"
+            }
+          ]
+        },
+        {
+            "code": "KA",
+            "state": "Karnataka",
+            "samples": [
+              {
+                "id": "#1",
+                "place": "Hogenakkal Falls",
+                "date": "2025-02-08",
+                "time": "13:30",
+                "latitude": 12.110985864583052,
+                "longitude": 77.77479544773149,
+                "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+              },
+              {
+                "id": "#2",
+                "place": "Bangalore",
+                "date": "2025-05-08",
+                "time": "16:30",
+                "latitude": 12.110985864583052,
+                "longitude": 77.77479544773149,
+                "notes": "From KR Garden 2nd Main, Murugeshpalya"
+              }
+            ]
+          },
+          {
+              "code": "KA",
+              "state": "Karnataka",
+              "samples": [
+                {
+                  "id": "#1",
+                  "place": "Hogenakkal Falls",
+                  "date": "2025-02-08",
+                  "time": "13:30",
+                  "latitude": 12.110985864583052,
+                  "longitude": 77.77479544773149,
+                  "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                },
+                {
+                  "id": "#2",
+                  "place": "Bangalore",
+                  "date": "2025-05-08",
+                  "time": "16:30",
+                  "latitude": 12.110985864583052,
+                  "longitude": 77.77479544773149,
+                  "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                }
+              ]
+            },
+            {
+                "code": "KA",
+                "state": "Karnataka",
+                "samples": [
+                  {
+                    "id": "#1",
+                    "place": "Hogenakkal Falls",
+                    "date": "2025-02-08",
+                    "time": "13:30",
+                    "latitude": 12.110985864583052,
+                    "longitude": 77.77479544773149,
+                    "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                  },
+                  {
+                    "id": "#2",
+                    "place": "Bangalore",
+                    "date": "2025-05-08",
+                    "time": "16:30",
+                    "latitude": 12.110985864583052,
+                    "longitude": 77.77479544773149,
+                    "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                  }
+                ]
+              },
+              {
+                  "code": "KA",
+                  "state": "Karnataka",
+                  "samples": [
+                    {
+                      "id": "#1",
+                      "place": "Hogenakkal Falls",
+                      "date": "2025-02-08",
+                      "time": "13:30",
+                      "latitude": 12.110985864583052,
+                      "longitude": 77.77479544773149,
+                      "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                    },
+                    {
+                      "id": "#2",
+                      "place": "Bangalore",
+                      "date": "2025-05-08",
+                      "time": "16:30",
+                      "latitude": 12.110985864583052,
+                      "longitude": 77.77479544773149,
+                      "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                    }
+                  ]
+                },
+                {
+                    "code": "KA",
+                    "state": "Karnataka",
+                    "samples": [
+                      {
+                        "id": "#1",
+                        "place": "Hogenakkal Falls",
+                        "date": "2025-02-08",
+                        "time": "13:30",
+                        "latitude": 12.110985864583052,
+                        "longitude": 77.77479544773149,
+                        "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                      },
+                      {
+                        "id": "#2",
+                        "place": "Bangalore",
+                        "date": "2025-05-08",
+                        "time": "16:30",
+                        "latitude": 12.110985864583052,
+                        "longitude": 77.77479544773149,
+                        "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                      }
+                    ]
+                  },
+                  {
+                      "code": "KA",
+                      "state": "Karnataka",
+                      "samples": [
+                        {
+                          "id": "#1",
+                          "place": "Hogenakkal Falls",
+                          "date": "2025-02-08",
+                          "time": "13:30",
+                          "latitude": 12.110985864583052,
+                          "longitude": 77.77479544773149,
+                          "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                        },
+                        {
+                          "id": "#2",
+                          "place": "Bangalore",
+                          "date": "2025-05-08",
+                          "time": "16:30",
+                          "latitude": 12.110985864583052,
+                          "longitude": 77.77479544773149,
+                          "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                        }
+                      ]
+                    },
+                    {
+                        "code": "KA",
+                        "state": "Karnataka",
+                        "samples": [
+                          {
+                            "id": "#1",
+                            "place": "Hogenakkal Falls",
+                            "date": "2025-02-08",
+                            "time": "13:30",
+                            "latitude": 12.110985864583052,
+                            "longitude": 77.77479544773149,
+                            "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                          },
+                          {
+                            "id": "#2",
+                            "place": "Bangalore",
+                            "date": "2025-05-08",
+                            "time": "16:30",
+                            "latitude": 12.110985864583052,
+                            "longitude": 77.77479544773149,
+                            "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                          }
+                        ]
+                      },{
+                            "code": "KA",
+                            "state": "Karnataka",
+                            "samples": [
+                              {
+                                "id": "#1",
+                                "place": "Hogenakkal Falls",
+                                "date": "2025-02-08",
+                                "time": "13:30",
+                                "latitude": 12.110985864583052,
+                                "longitude": 77.77479544773149,
+                                "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                              },
+                              {
+                                "id": "#2",
+                                "place": "Bangalore",
+                                "date": "2025-05-08",
+                                "time": "16:30",
+                                "latitude": 12.110985864583052,
+                                "longitude": 77.77479544773149,
+                                "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                              }
+                            ]
+                          },{
+                                "code": "KA",
+                                "state": "Karnataka",
+                                "samples": [
+                                  {
+                                    "id": "#1",
+                                    "place": "Hogenakkal Falls",
+                                    "date": "2025-02-08",
+                                    "time": "13:30",
+                                    "latitude": 12.110985864583052,
+                                    "longitude": 77.77479544773149,
+                                    "notes": "Collected in the middle of Kaveri River while enjoying Coracle ride"
+                                  },
+                                  {
+                                    "id": "#2",
+                                    "place": "Bangalore",
+                                    "date": "2025-05-08",
+                                    "time": "16:30",
+                                    "latitude": 12.110985864583052,
+                                    "longitude": 77.77479544773149,
+                                    "notes": "From KR Garden 2nd Main, Murugeshpalya"
+                                  }
+                                ]
+                              },
+
+
+  {
+    "code": "KL",
+    "state": "Kerala",
+    "samples": [
+      {
+        "id": "#1",
+        "place": "Kannur Drive In Beach",
+        "date": "2025-04-19",
+        "time": "16:58",
+        "latitude": 11.808011158163934,
+        "longitude": 75.43246607386708,
+        "notes": "Collected from Muzhappilangad Drive In Beach"
+      }
+    ]
+  }
+];
+
+const carousel = document.getElementById('state-carousel');
+const sampleSection = document.getElementById('sample-section');
+
+// Load State Carousel
+data.forEach((state, index) => {
+  const tile = document.createElement('div');
+  tile.className = 'state-tile';
+  tile.innerHTML = `
+    <img src="./images/states/${state.code}.png" alt="${state.state}" class="state-map">
+    <div class="fw-bold">${state.state}</div>
+  `;
+  tile.addEventListener('click', () => {
+    document.querySelectorAll('.state-tile').forEach(el => el.classList.remove('active'));
+    tile.classList.add('active');
+    loadSamples(state);
+  });
+  if (index === 0) {
+    tile.classList.add('active');
+    loadSamples(state);
+  }
+  carousel.appendChild(tile);
+});
+
+// Load Samples Function
+function loadSamples(stateData) {
+  sampleSection.innerHTML = ''; // clear
+  if(stateData.samples.length == 0) {
+    const tile = document.createElement('div');
+    tile.className = 'no-record d-flex  align-items-center justify-content-center fs-1 fw-bold';
+    tile.innerHTML = `Hope to visit soon...`;
+    sampleSection.appendChild(tile);
+  } else {
+      stateData.samples.forEach(sample => {
+        const tile = document.createElement('div');
+        tile.className = 'sample-tile';
+        tile.innerHTML = `
+          <img src="./images/places/drivein.png" alt="${sample.place}">
+          <div class="sample-content">
+            <div class="sample-header">
+              <h3>${sample.place}</h3>
+              <div class="sample-id">${sample.id}</div>
+            </div>
+            <div class="text-black-50 small">${stateData.state}</div>
+            <div class="sample-meta">
+            <div class="d-flex align-items-center justify-content-between">
+                <div>
+                    <i class="bi bi-calendar"></i>
+                    ${sample.date}
+                  </div>
+                  <div>
+                    <i class="bi bi-alarm"></i>
+                    ${sample.time}
+                  </div>
+            </div>
+
+              <div>
+                <i class="bi bi-geo-alt"></i>
+                <a href="https://www.google.com/maps?q=${sample.latitude},${sample.longitude}" target="_blank">View on Map</a>
+              </div>
+            </div>
+            <div class="sample-notes align-items-start">
+            <i class="bi bi-card-text"></i>
+            <div class="ps-2">${sample.notes}</div>
+            </div>
+          </div>
+        `;
+        sampleSection.appendChild(tile);
+      });
+    }
+}
